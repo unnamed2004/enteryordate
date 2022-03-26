4c65c50f91c64082155f42a20d9e94ced6a5ef23
umar gafurov <rokarokaev@gmail.com>
2022-03-26 16:39:44 +0300

    Update savol.cpp

--- savol.cpp
+++ savol.cpp
@@ -15,8 +15,8 @@ int main()
     age=2022-year;
 
     if(age>17 && age<45)
-        cout<< "Hello Padanalat"<<name<<" "<<surname<<", you have been hired.";
+        cout<< "siyvusha uvazheni"<<name<<" "<<surname<<", you have been hired.";
     else
-        cout<<"Hello Bepadar"<<name<<surname<<", you have not been hired.";
+        cout<<"Hello siyvushi Bepadar"<<name<<surname<<", you have not been hired.";
 
 }
